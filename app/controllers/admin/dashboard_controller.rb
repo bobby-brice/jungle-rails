e5dffce152608fd2ec5074afac9441525cb43e17
bobby-brice <bobby.brice@gmail.com>
2020-07-22 22:40:55 +0000

    add admin dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with username: ENV["USER_NAME"], password: ENV["PASSWORD"]
+  http_basic_authenticate_with name: ENV["USER_NAME"], password: ENV["PASSWORD"]
+  
   def show
+    puts ENV["USER_NAME"]
+    puts ENV["PASSWORD"]
+
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
   end
 end
